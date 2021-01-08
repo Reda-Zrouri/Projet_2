ca8728af634fa8e1c8b2c73f9a89bbcc0fd3527a
Reda Zrouri <reda.zrouri@hotmail.fr>
2021-01-08 18:04:27 +0100

    extraction de donner produit et titre

--- open.py
+++ open.py
@@ -1,11 +1,18 @@
+#import module
+
 import requests 
 from bs4 import BeautifulSoup
 
+
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-reponse = requests.get (url)
+reponse = requests.get(url)
 if reponse.ok: 
 	soup = BeautifulSoup(reponse.text, 'lxml')
-	titre = soup.find('article', {'class': 'product_page'}).find('h1')
-	production = soup.find('table')
-	print("titre: " + titre.text + " Production Information: " + production.text)
+	tds = soup.findAll('tr')
+	title = soup.find('title')
+
+	print(title.text)
+	print(tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text, tds[5].text, tds[6].text)
+
+
 	
